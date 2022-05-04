bf1cb06389c5ba417e24f1033ad02aace79b0ff7
BKORBA10 <b.korban.10@student.scu.edu.au>
2022-05-04 12:48:40 +1000

    first commit

--- challenge1.js
+++ challenge1.js
@@ -1,7 +1,8 @@
 //Get the nth number in the fibonacci sequence given n
+//formula F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1
+//in the following integer sequence 0,1,1,2,3 the next number will be x = 3 + 2; x = 5
 
-
-let n = 1;
+let n = 10;
 
 // function returns the Fibonacci number
 function fibonacci(n) {
--- challenge2.js
+++ challenge2.js
@@ -1,28 +1,27 @@
+//hash array size as 256
 let ASCII_SIZE = 256;
-function getMaxOccurringChar(str)
+function getTheHighestNumberOfOccurences(str)
     {
 
-        // Create array to keep the count of individual
-        // characters and initialize the array as 0
+        //an array to keep the count of individual characters
+        //array initialise as 0
         let count = new Array(ASCII_SIZE);
         for (let i = 0; i < ASCII_SIZE; i++)
         {
             count[i] = 0;
         }
 
-        // Construct character count array from the input
-        // string.
-        let len = str.length;
-        for (let i = 0; i < len; i++)
+        // array of characters constructed from the input string
+        let length  = str.length;
+        for (let i = 0; i < length; i++)
         {
             count[str[i].charCodeAt(0)] += 1;
         }
         let max = -1;   // Initialize max count
         let result = ' ';   // Initialize result
 
-        // Traversing through the string and maintaining
-        // the count of each character
-        for (let i = 0; i < len; i++)
+        // track the string and maintaint the count of each character
+        for (let i = 0; i < length; i++)
         {
             if (max < count[str[i].charCodeAt(0)])
             {
@@ -33,7 +32,7 @@ function getMaxOccurringChar(str)
         return result;
     }
 
-    // Driver Method
-    let str = "challenge";
-    console.log("Max occurring character is " , getMaxOccurringChar(str));
+    //check the occurrence
+    let str = "impossible";
+    console.log("Max occurring character is " , getTheHighestNumberOfOccurences(str));
 
